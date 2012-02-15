32cd2441991372983c0a070c536de7cd42392965
Renato <Renato@renato-mac.local>
2012-02-15 09:55:32 -0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ralencar::Application.config.secret_token = '59e69aa6deb14d8155212542973f9a1bd8be51367951e091aba1e894a8c5f452d54122164dedd5e5711635f6effa0ab426dc1a0045e05133fe6b71c610669ff7'
